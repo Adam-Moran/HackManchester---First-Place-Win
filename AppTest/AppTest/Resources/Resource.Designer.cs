fb27d925b18205f7421faed60c31bc6e83488afd
Adam Moran <adam.moran57@gmail.com>
2017-10-28 17:55:21 +0100

    fixed bug

--- AppTest/AppTest/Resources/Resource.Designer.cs
+++ AppTest/AppTest/Resources/Resource.Designer.cs
@@ -50,11 +50,11 @@ namespace AppTest
 			// aapt resource value: 0x7f040002
 			public const int contentFrame = 2130968578;
 			
-			// aapt resource value: 0x7f040001
-			public const int rqReader = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int textView1 = 2130968576;
+			public const int rqReader = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int textView1 = 2130968577;
 			
 			static Id()
 			{
