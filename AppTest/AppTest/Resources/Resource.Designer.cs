9e7735174585b77257f360584acae4ce70d96a22
Alan Ditchfield <aditchfield1@ombudsman-services.org>
2017-10-28 21:52:44 +0100

    Shopping Basket fixes

--- AppTest/AppTest/Resources/Resource.Designer.cs
+++ AppTest/AppTest/Resources/Resource.Designer.cs
@@ -69,11 +69,11 @@ namespace AppTest
 			// aapt resource value: 0x7f050005
 			public const int contentFrame = 2131034117;
 			
-			// aapt resource value: 0x7f050003
-			public const int finished = 2131034115;
-			
 			// aapt resource value: 0x7f050002
-			public const int hiddenField = 2131034114;
+			public const int finished = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int hiddenField = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int imageView1 = 2131034112;
