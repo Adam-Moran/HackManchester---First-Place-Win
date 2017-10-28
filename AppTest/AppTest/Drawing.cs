4f0630459e6f5aaa60eb1e4808a1832af5ebf605
Alan Ditchfield <aditchfield1@ombudsman-services.org>
2017-10-28 23:38:53 +0100

    Added bean monster

--- AppTest/AppTest/Drawing.cs
+++ AppTest/AppTest/Drawing.cs
@@ -20,27 +20,41 @@ namespace AppTest
     {
         private readonly ShapeDrawable _shape;
         private Bitmap bg;
+        private Bitmap monster;
         private Paint paint;
         public Drawing(Context context) : base(context)
         {
             bg = BitmapFactory.DecodeResource(context.Resources, Resource.Drawable.BackgroundSmall);
+            monster = BitmapFactory.DecodeResource(context.Resources, Resource.Drawable.beans);
+
+            //configure Paint
             paint = new Paint();
             paint.SetARGB(255, 200, 255, 0);
             paint.SetStyle(Paint.Style.Stroke);
             paint.StrokeWidth = 4;
+
+            //Squid's Oval stuff
             _shape = new ShapeDrawable(new OvalShape());
             _shape.Paint.Set(paint);
-
             _shape.SetBounds(20, 20, 300, 200);
+
+
         }
 
         protected override void OnDraw(Canvas canvas)
         {
-            Rect rs = new Rect(0, 0, bg.Width, bg.Height);
-            Rect rd= new Rect(0, 0, canvas.Width, canvas.Height);
-            _shape.Draw(canvas);
-            canvas.DrawBitmap(bg, rs, rd, new Paint());
+            Rect bg_rs = new Rect(0, 0, bg.Width, bg.Height);
+            Rect bg_rd = new Rect(0, 0, canvas.Width, canvas.Height);
 
+            Rect monst_rs = new Rect(0, 0, monster.Width, monster.Height);
+            Rect monst_rd = new Rect(canvas.Width/4, canvas.Height/2, canvas.Width / 2 + canvas.Width/4, canvas.Height);
+
+            //draw background
+            canvas.DrawBitmap(bg, bg_rs, bg_rd, paint);
+
+            //draw monster
+            canvas.DrawBitmap(monster, monst_rs, monst_rd, paint);
+            _shape.Draw(canvas);
         }
 
     }
