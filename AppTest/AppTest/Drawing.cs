9bc115a5816e2a78a79927e4afc5494988ec1aae
Adam Myers <AdamMyers@hotmail.co.uk>
2017-10-28 21:43:36 +0100

    Adding drawing class

---
+++ AppTest/AppTest/Drawing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AppTest
+{
+    class Drawing : View
+    {
+        private readonly ShapeDrawable _shape;
+        public Drawing(Context context) : base(context)
+        {
+            var paint = new Paint();
+            paint.SetARGB(255, 200, 255, 0);
+            paint.SetStyle(Paint.Style.Stroke);
+            paint.StrokeWidth = 4;
+
+            _shape = new ShapeDrawable(new OvalShape());
+            _shape.Paint.Set(paint);
+
+            _shape.SetBounds(20, 20, 300, 200);
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            _shape.Draw(canvas);
+        }
+
+    }
+}
