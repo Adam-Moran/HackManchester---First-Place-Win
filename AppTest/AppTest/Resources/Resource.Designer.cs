79be3626dd4226cb24ece6e1f71919e2a7a496c2
Alan Ditchfield <aditchfield1@ombudsman-services.org>
2017-10-28 18:12:01 +0100

    Added BEANS

--- AppTest/AppTest/Resources/Resource.Designer.cs
+++ AppTest/AppTest/Resources/Resource.Designer.cs
@@ -44,14 +44,33 @@ namespace AppTest
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int beans = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int contentFrame = 2130968577;
+			// aapt resource value: 0x7f050002
+			public const int contentFrame = 2131034114;
 			
-			// aapt resource value: 0x7f040000
-			public const int rqReader = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int rqReader = 2131034113;
 			
 			static Id()
 			{
@@ -66,14 +85,14 @@ namespace AppTest
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
-			// aapt resource value: 0x7f020001
-			public const int zxingscanneractivitylayout = 2130837505;
+			// aapt resource value: 0x7f030001
+			public const int zxingscanneractivitylayout = 2130903041;
 			
-			// aapt resource value: 0x7f020002
-			public const int zxingscannerfragmentlayout = 2130837506;
+			// aapt resource value: 0x7f030002
+			public const int zxingscannerfragmentlayout = 2130903042;
 			
 			static Layout()
 			{
@@ -88,8 +107,8 @@ namespace AppTest
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
