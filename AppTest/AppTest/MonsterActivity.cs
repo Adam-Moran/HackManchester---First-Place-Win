339b51e34a9d9d9d1ab9c31417a91a5767b16c35
Alan Ditchfield <aditchfield1@ombudsman-services.org>
2017-10-28 20:56:05 +0100

    Pass Barcode list to MonsterActivity

--- AppTest/AppTest/MonsterActivity.cs
+++ AppTest/AppTest/MonsterActivity.cs
@@ -17,9 +17,11 @@ namespace AppTest
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Monster);
+            TextView field = FindViewById<TextView>(Resource.Id.textView1);
+            field.Text = Intent.GetStringExtra("Barcodes") ?? "Data not available";
+            
         }
     }
 }
