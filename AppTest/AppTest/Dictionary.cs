1487a660873928a0c680dada0a59c8b79d2c66c5
Adam Myers <AdamMyers@hotmail.co.uk>
2017-10-29 04:00:08 +0000

    j

---
+++ AppTest/AppTest/Dictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AppTest
+{
+    class Dictionary
+    {
+        public Dictionary<string, object> productDictionary = new Dictionary<string, object>
+        {
+            { "510555260112", new {name = "Water", image = Resource.Drawable.water}},
+            { "5000159500920", new {name = "M&M's", image = Resource.Drawable.water}},
+            { "5000159503938", new {name = "Revels", image = Resource.Drawable.water}},
+            { "5000159458290", new {name = "Galaxy", image = Resource.Drawable.water}},
+            { "5020379129565", new {name = "Ginger Nuts", image = Resource.Drawable.water}},
+            { "5054268158985", new {name = "Chocolate Cookies", image = Resource.Drawable.water}},
+            { "5060292307909", new {name = "Star Wars Puffs", image = Resource.Drawable.water}},
+            { "5054267000216", new {name = "Lucozade", image = Resource.Drawable.water}},
+            { "3329770056923", new {name = "Froobe", image = Resource.Drawable.water}},
+            { "5030765032904", new {name = "Caramel Chocolate", image = Resource.Drawable.water}},
+            { "7613033591877", new {name = "Blue Ribbon", image = Resource.Drawable.water}},
+            { "Key1", new {name = "haribo", image = Resource.Drawable.haribobears}},
+            { "7622210723512", new {name = "Chomp", image = Resource.Drawable.water}},
+            { "8008440222008", new {name = "Peroni", image = Resource.Drawable.water}},
+            { "5020379129541", new {name = "Fruit Shortcake", image = Resource.Drawable.water}},
+            { "Key1", new {name = "Redbull", image = Resource.Drawable.redbull}},
+            { "Key1", new {name = "Pasta", image = Resource.Drawable.pasta}},
+            { "Key1", new {name = "Cereal", image = Resource.Drawable.Cereal}},
+            { "Key1", new {name = "Name1", image = Resource.Drawable.water}},
+        };
+    }
+}
